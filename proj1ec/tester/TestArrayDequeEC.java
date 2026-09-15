d05d4961b7d0a297bcc1ad6c1e1ef101bf083c0d
Junjie Mei <roderick.login@gmail.com>
2026-09-15 17:38:54 +0800

    fix some code to pass style check

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,45 @@
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+import edu.princeton.cs.introcs.StdRandom;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void randomizedTest() {
+        StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
+
+        String message = "";
+
+        while (true) {
+            int operationNum = StdRandom.uniform(4);
+            if (operationNum == 0) {
+                int value = StdRandom.uniform(100);
+
+                student.addFirst(value);
+                solution.addFirst(value);
+                message += "addFirst" + "(" + value + ")\n";
+            } else if (operationNum == 1) {
+                int value = StdRandom.uniform(100);
+                student.addLast(value);
+                solution.addLast(value);
+                message += "addLast" + "(" + value + ")\n";
+            } else if (operationNum == 2) {
+                Integer studentResult = student.removeFirst();
+                Integer solutionResult = solution.removeFirst();
+                message += "removeFirst()\n";
+                assertEquals(message, solutionResult, studentResult);
+            } else if (operationNum == 3) {
+                Integer studentResult = student.removeLast();
+                Integer solutionResult = solution.removeLast();
+                message += "removeLast()\n";
+                assertEquals(message, solutionResult, studentResult);
+            }
+        }
+    }
+
+}
